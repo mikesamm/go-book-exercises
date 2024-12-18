b01bdb6b3c79636b57922bb0c67706a750a70ea5
Mike Sammartino <sammartino.mike@gmail.com>
2024-12-17 22:09:55 -0600

    append write to file, set correct file permissions

--- write/main.go
+++ write/main.go
@@ -6,9 +6,28 @@ import (
 )
 
 func main() {
+	// write an entire text file, overwrite if file already there
 	txt := []byte("Here's a multiple line text file\nso that I can practice writing\nfiles with the GO book exercises.\nAnother line for kicks.")
-	err := os.WriteFile("/Users/mike/Documents/apps/go-easy-book/sampleWrite.txt", txt, 0644)
+	err := os.WriteFile("/Users/mike/Documents/apps/go-easy-book/sampleWrite.txt", txt, 0666)
 	check(err)
+
+	// open the same file for an append operation
+	file, err := os.OpenFile("/Users/mike/Documents/apps/go-easy-book/sampleWrite.txt", 0666, os.FileMode(os.O_APPEND))
+	check(err)
+	// close file at end of function after other operations finish
+	defer file.Close()
+
+	// append text to the file content and check for errors
+	addText := []byte("\nby Extreme Lord Mastermind Dave")
+	// get total number of bytes to know where to start write
+	totalBytes, err := os.ReadFile("/Users/mike/Documents/apps/go-easy-book/sampleWrite.txt")
+	check(err)
+	lenTotalBytes := len(totalBytes)
+	bytesWritten, err := file.WriteAt(addText, int64(lenTotalBytes))
+	check(err)
+
+	// display number of chars written and what has been written
+	fmt.Printf("%v chars appended: %v", bytesWritten, string(addText[:bytesWritten]))
 }
 
 func check(err error) {
